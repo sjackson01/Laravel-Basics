60f0be4f97cc655e8b6c5fc61a7c74cf8d65bb06
sjackson01 <stevenpjackson0@gmail.com>
2020-09-19 14:04:58 -0500

    feat(controller): create test laravel controllers

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'AppController@index');
 
 Route::get('/tracks', function () {
     return view('welcome');
