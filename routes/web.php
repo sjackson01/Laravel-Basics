74aa194585c2bdf6c1322aea66574b377f156192
sjackson01 <stevenpjackson0@gmail.com>
2020-09-21 16:27:54 -0500

    feat(blade): build out navigation with additional blades and references

--- routes/web.php
+++ routes/web.php
@@ -16,13 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'AppController@index');
 
 Route::get('/tracks', function () {
-    return view('welcome');
+    return view('tracks');
 });
 
 Route::get('/community', function () {
-    return view('welcome');
+    return view('community');
 });
 
 Route::get('/support', function () {
-    return view('welcome');
+    return view('support');
 });
